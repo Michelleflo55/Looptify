63664297af130a90f1eedc66192903e7ee60bad5
Michelle Flores <lmfg2456@gmail.com>
2022-08-23 17:40:24 -0700

    completed GET

--- Assessment/controllers/AppointmentController.js
+++ Assessment/controllers/AppointmentController.js
@@ -1,16 +1,56 @@
 const { Appointment} = require('../models')
 
+//http://localhost:3001/appointments
 const GetAppointments = async (req, res) => {
    try {
     const appointments = await Appointment.findAll()
         res.send(appointments)
    } catch (error) {
-    
+    throw error
    }
+}
+
+//http://localhost:3001/appointments/1
+const GetAppointment = async (req, res) =>{
+    try {
+       const appointment = await Appointment.findByPk(req.params.id) 
+       res.send(appointment)
+    } catch (error) {
+        throw error
+       
+    }
+}
+
+// const PostAppointment = async (req, res) =>{
+//     try {
+//         const newAppointment = await Appointment.create(req.body)
+//         await newAppointment.save()
+//         res.send(newAppointment)
+//     } catch (error) {
+        
+//     }
+// }
+const UpdateAppointment = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const DeleteAppointment = async (req, res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
 }
 
 
 
+
 module.exports = {
-    GetAppointments
+    GetAppointments,
+    GetAppointment,
+    // PostAppointment
 }
--- Assessment/routes/AppointmentRouter.js
+++ Assessment/routes/AppointmentRouter.js
@@ -2,5 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/AppointmentController')
 
 Router.get('/', controller.GetAppointments)
+Router.get('/:id', controller.GetAppointment)
+// Router.post('/newAppointment', controller.PostAppointment)
 
 module.exports = Router
